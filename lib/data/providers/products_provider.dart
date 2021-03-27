ea719e3b4b16787756dc8740edd40bc2d6bb7667
Rares Cruceat <cruceat.rares@gmail.com>
2021-03-27 20:56:42 +0200

    add filter by favorites feature

--- lib/data/providers/products_provider.dart
+++ lib/data/providers/products_provider.dart
@@ -37,7 +37,17 @@ class ProductsProvider with ChangeNotifier {
     ),
   ];
 
+  bool _showFavoritesOnly = false;
+
+  void filterByFavorites(bool onlyFavorites) {
+    _showFavoritesOnly = onlyFavorites;
+    notifyListeners();
+  }
+
   List<Product> get products {
+    if (_showFavoritesOnly) {
+      return _products.where((element) => element.isFavorite).toList();
+    }
     return [..._products];
   }
 
--- lib/screens/products_list_screen.dart
+++ lib/screens/products_list_screen.dart
@@ -1,14 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:my_shop_app/components/products_grid_list.dart';
+import 'package:my_shop_app/data/providers/products_provider.dart';
+import 'package:provider/provider.dart';
 
 class ProductsListScreen extends StatelessWidget {
   static const String route = '/products';
 
+  void changeShowFavoritesFilter(
+      ProductsProvider productsProvider, bool onlyFavorites) {
+    productsProvider.filterByFavorites(onlyFavorites);
+  }
+
   @override
   Widget build(BuildContext context) {
+    final ProductsProvider productsProvider =
+        Provider.of<ProductsProvider>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('My Shop'),
+        actions: [
+          PopupMenuButton(
+            onSelected: (int selectedValue) {
+              changeShowFavoritesFilter(productsProvider, selectedValue == 0);
+            },
+            icon: Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              PopupMenuItem(child: Text('Only Favorites'), value: 0),
+              PopupMenuItem(child: Text('Show All'), value: 1)
+            ],
+          ),
+        ],
       ),
       body: ProductsGridList(),
     );
