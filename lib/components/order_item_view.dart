da9f90db282fd7890deb0665d779aa84a9a3b1cf
Rares Cruceat <cruceat.rares@gmail.com>
2021-03-28 10:32:07 +0200

    show order product items

--- lib/components/order_item_view.dart
+++ lib/components/order_item_view.dart
@@ -3,7 +3,20 @@ import 'package:my_shop_app/data/models/order.dart';
 import 'package:provider/provider.dart';
 import 'package:intl/intl.dart';
 
-class OrderItemView extends StatelessWidget {
+class OrderItemView extends StatefulWidget {
+  @override
+  _OrderItemViewState createState() => _OrderItemViewState();
+}
+
+class _OrderItemViewState extends State<OrderItemView> {
+  bool _isExpanded = false;
+
+  void toggleExpand() {
+    setState(() {
+      _isExpanded = !_isExpanded;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final Order order = Provider.of<Order>(context);
@@ -11,16 +24,46 @@ class OrderItemView extends StatelessWidget {
       margin: EdgeInsets.all(10),
       child: Column(
         children: [
-          ListTile(
-            title: Text('\$${order.amount}'),
-            subtitle: Text(
-              DateFormat('dd MM yyyy hh:mm').format(order.dateTime),
-            ),
-            trailing: IconButton(
-              icon: Icon(Icons.more),
-              onPressed: () {},
+          Container(
+            color: Theme.of(context).primaryColor,
+            child: ListTile(
+              title: Text(
+                '\$${order.amount}',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+              subtitle: Text(
+                DateFormat('dd/MM/yyyy hh:mm').format(order.dateTime),
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              trailing: IconButton(
+                icon: Icon(
+                  _isExpanded ? Icons.expand_less : Icons.expand_more,
+                  color: Colors.white,
+                ),
+                onPressed: toggleExpand,
+              ),
             ),
           ),
+          if (_isExpanded)
+            Column(
+              children: [
+                ...order.products
+                    .map(
+                      (e) => ListTile(
+                        title: Text(e.title),
+                        subtitle: Text('Q: ${e.quantity} | \$${e.price}'),
+                        trailing: Text('${e.quantity * e.price}'),
+                      ),
+                    )
+                    .toList()
+              ],
+            ),
         ],
       ),
     );
