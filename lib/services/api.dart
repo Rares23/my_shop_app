b442b18960fcee8efa832fa5222b3abb3d97daf7
Rares Cruceat <cruceat.rares@gmail.com>
2021-03-31 07:59:14 +0200

    integrate a minimal api service - not working

---
+++ lib/services/api.dart
@@ -0,0 +1,12 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class Api {
+  void request() {
+    String url =
+        'https://myshop-dd26b-default-rtdb.europe-west1.firebasedatabase.app/';
+
+    Uri uri = Uri.parse(url, null);
+    http.post(uri, body: json.encode({})).then((response) => {});
+  }
+}
